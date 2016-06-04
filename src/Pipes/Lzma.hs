ab22594124affab7348a2038b554f6a92ed53df3
Ben Gamari <ben@smart-cactus.org>
2016-06-04 12:12:09 +0200

    Fix handling of empty chunks

--- src/Pipes/Lzma.hs
+++ src/Pipes/Lzma.hs
@@ -5,6 +5,7 @@ module Pipes.Lzma ( compress, decompress ) where
 import Pipes
 import qualified Codec.Compression.Lzma as Lzma
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
 
 decompress :: forall m r. MonadIO m
            => Producer ByteString m r
@@ -14,16 +15,20 @@ decompress prod0 = liftIO (Lzma.decompressIO Lzma.defaultDecompressParams) >>= g
     go :: Producer ByteString m r
        -> Lzma.DecompressStream IO
        -> Producer ByteString m (Producer ByteString m r)
-    go prod (Lzma.DecompressInputRequired more) = do
+    go prod s@(Lzma.DecompressInputRequired more) = do
         mx <- lift $ next prod
         case mx of
-          Right (x, prod') -> liftIO (more x) >>= go prod'
+          Right (x, prod')
+            | BS.null x    -> go prod' s
+            | otherwise    -> liftIO (more x) >>= go prod'
           Left r           -> liftIO (more mempty) >>= go (return r)
     go prod (Lzma.DecompressOutputAvailable output cont) = do
         yield output
         liftIO cont >>= go prod
     go prod (Lzma.DecompressStreamEnd leftover) =
         return (yield leftover >> prod)
+    go prod (Lzma.DecompressStreamError Lzma.LzmaRetOK) =
+        return prod
     go _prod (Lzma.DecompressStreamError err) =
         fail $ "Pipes.Lzma.decompress: Error "++show err
 
@@ -36,10 +41,12 @@ compress prod0 = liftIO (Lzma.compressIO Lzma.defaultCompressParams) >>= go prod
     go :: Producer ByteString m r
        -> Lzma.CompressStream IO
        -> Producer ByteString m r
-    go prod (Lzma.CompressInputRequired _flush more) = do
+    go prod s@(Lzma.CompressInputRequired _flush more) = do
         mx <- lift $ next prod
         case mx of
-          Right (x, prod') -> liftIO (more x) >>= go prod'
+          Right (x, prod')
+            | BS.null x    -> go prod' s
+            | otherwise    -> liftIO (more x) >>= go prod'
           Left r           -> liftIO (more mempty) >>= go (return r)
     go prod (Lzma.CompressOutputAvailable output cont) = do
         yield output
